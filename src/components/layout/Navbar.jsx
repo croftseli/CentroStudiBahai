883492ba1a0a76c9d180132fc1959cc20bc7cbb9
MichaelSanei <michaelnabil.sanei@gmail.com>
2025-05-11 21:11:40 +0100

    new logo

--- src/components/layout/Navbar.jsx
+++ src/components/layout/Navbar.jsx
@@ -42,9 +42,9 @@ export default function Navbar() {
       { href: "/it/surroundings", label: "Dintorni" },
       { href: "/it/activities", label: "Servizi" },
       { href: "/it/contactUs", label: "Contattaci" },
-    ]
+    ],
   };
-  
+
   // Get current language navigation links
   const navLinks = navLinksTranslations[language];
 
@@ -55,8 +55,9 @@ export default function Navbar() {
 
   // Language Toggle Slider component
   const LanguageToggleSlider = () => {
-    const TOGGLE_CLASSES = "text-sm font-medium flex items-center px-3 md:pl-3 md:pr-3.5 py-2 md:py-1.5 transition-colors relative z-10";
-    
+    const TOGGLE_CLASSES =
+      "text-sm font-medium flex items-center px-3 md:pl-3 md:pr-3.5 py-2 md:py-1.5 transition-colors relative z-10";
+
     return (
       <div className="relative flex w-fit items-center rounded-full border border-gray-200">
         <button
@@ -65,20 +66,66 @@ export default function Navbar() {
           }`}
           onClick={() => handleLanguageToggle("en")}
         >
-          <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 32 32" className="relative z-10">
-            <rect x="1" y="4" width="30" height="24" rx="4" ry="4" fill="#071b65"></rect>
-            <path d="M5.101,4h-.101c-1.981,0-3.615,1.444-3.933,3.334L26.899,28h.101c1.981,0,3.615-1.444,3.933-3.334L5.101,4Z" fill="#fff"></path>
-            <path d="M22.25,19h-2.5l9.934,7.947c.387-.353,.704-.777,.929-1.257l-8.363-6.691Z" fill="#b92932"></path>
-            <path d="M1.387,6.309l8.363,6.691h2.5L2.316,5.053c-.387,.353-.704,.777-.929,1.257Z" fill="#b92932"></path>
-            <path d="M5,28h.101L30.933,7.334c-.318-1.891-1.952-3.334-3.933-3.334h-.101L1.067,24.666c.318,1.891,1.952,3.334,3.933,3.334Z" fill="#fff"></path>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="24"
+            height="24"
+            viewBox="0 0 32 32"
+            className="relative z-10"
+          >
+            <rect
+              x="1"
+              y="4"
+              width="30"
+              height="24"
+              rx="4"
+              ry="4"
+              fill="#071b65"
+            ></rect>
+            <path
+              d="M5.101,4h-.101c-1.981,0-3.615,1.444-3.933,3.334L26.899,28h.101c1.981,0,3.615-1.444,3.933-3.334L5.101,4Z"
+              fill="#fff"
+            ></path>
+            <path
+              d="M22.25,19h-2.5l9.934,7.947c.387-.353,.704-.777,.929-1.257l-8.363-6.691Z"
+              fill="#b92932"
+            ></path>
+            <path
+              d="M1.387,6.309l8.363,6.691h2.5L2.316,5.053c-.387,.353-.704,.777-.929,1.257Z"
+              fill="#b92932"
+            ></path>
+            <path
+              d="M5,28h.101L30.933,7.334c-.318-1.891-1.952-3.334-3.933-3.334h-.101L1.067,24.666c.318,1.891,1.952,3.334,3.933,3.334Z"
+              fill="#fff"
+            ></path>
             <rect x="13" y="4" width="6" height="24" fill="#fff"></rect>
             <rect x="1" y="13" width="30" height="6" fill="#fff"></rect>
             <rect x="14" y="4" width="4" height="24" fill="#b92932"></rect>
-            <rect x="14" y="1" width="4" height="30" transform="translate(32) rotate(90)" fill="#b92932"></rect>
-            <path d="M28.222,4.21l-9.222,7.376v1.414h.75l9.943-7.94c-.419-.384-.918-.671-1.471-.85Z" fill="#b92932"></path>
-            <path d="M2.328,26.957c.414,.374,.904,.656,1.447,.832l9.225-7.38v-1.408h-.75L2.328,26.957Z" fill="#b92932"></path>
-            <path d="M27,4H5c-2.209,0-4,1.791-4,4V24c0,2.209,1.791,4,4,4H27c2.209,0,4-1.791,4-4V8c0-2.209-1.791-4-4-4Zm3,20c0,1.654-1.346,3-3,3H5c-1.654,0-3-1.346-3-3V8c0-1.654,1.346-3,3-3H27c1.654,0,3,1.346,3,3V24Z" opacity=".15"></path>
-            <path d="M27,5H5c-1.657,0-3,1.343-3,3v1c0-1.657,1.343-3,3-3H27c1.657,0,3,1.343,3,3v-1c0-1.657-1.343-3-3-3Z" fill="#fff" opacity=".2"></path>
+            <rect
+              x="14"
+              y="1"
+              width="4"
+              height="30"
+              transform="translate(32) rotate(90)"
+              fill="#b92932"
+            ></rect>
+            <path
+              d="M28.222,4.21l-9.222,7.376v1.414h.75l9.943-7.94c-.419-.384-.918-.671-1.471-.85Z"
+              fill="#b92932"
+            ></path>
+            <path
+              d="M2.328,26.957c.414,.374,.904,.656,1.447,.832l9.225-7.38v-1.408h-.75L2.328,26.957Z"
+              fill="#b92932"
+            ></path>
+            <path
+              d="M27,4H5c-2.209,0-4,1.791-4,4V24c0,2.209,1.791,4,4,4H27c2.209,0,4-1.791,4-4V8c0-2.209-1.791-4-4-4Zm3,20c0,1.654-1.346,3-3,3H5c-1.654,0-3-1.346-3-3V8c0-1.654,1.346-3,3-3H27c1.654,0,3,1.346,3,3V24Z"
+              opacity=".15"
+            ></path>
+            <path
+              d="M27,5H5c-1.657,0-3,1.343-3,3v1c0-1.657,1.343-3,3-3H27c1.657,0,3,1.343,3,3v-1c0-1.657-1.343-3-3-3Z"
+              fill="#fff"
+              opacity=".2"
+            ></path>
           </svg>
           <span className="relative z-10 ml-1">English</span>
         </button>
@@ -88,12 +135,32 @@ export default function Navbar() {
           }`}
           onClick={() => handleLanguageToggle("it")}
         >
-          <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 32 32" className="relative z-10">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="24"
+            height="24"
+            viewBox="0 0 32 32"
+            className="relative z-10"
+          >
             <path fill="#fff" d="M10 4H22V28H10z"></path>
-            <path d="M5,4h6V28H5c-2.208,0-4-1.792-4-4V8c0-2.208,1.792-4,4-4Z" fill="#41914d"></path>
-            <path d="M25,4h6V28h-6c-2.208,0-4-1.792-4-4V8c0-2.208,1.792-4,4-4Z" transform="rotate(180 26 16)" fill="#bf393b"></path>
-            <path d="M27,4H5c-2.209,0-4,1.791-4,4V24c0,2.209,1.791,4,4,4H27c2.209,0,4-1.791,4-4V8c0-2.209-1.791-4-4-4Zm3,20c0,1.654-1.346,3-3,3H5c-1.654,0-3-1.346-3-3V8c0-1.654,1.346-3,3-3H27c1.654,0,3,1.346,3,3V24Z" opacity=".15"></path>
-            <path d="M27,5H5c-1.657,0-3,1.343-3,3v1c0-1.657,1.343-3,3-3H27c1.657,0,3,1.343,3,3v-1c0-1.657-1.343-3-3-3Z" fill="#fff" opacity=".2"></path>
+            <path
+              d="M5,4h6V28H5c-2.208,0-4-1.792-4-4V8c0-2.208,1.792-4,4-4Z"
+              fill="#41914d"
+            ></path>
+            <path
+              d="M25,4h6V28h-6c-2.208,0-4-1.792-4-4V8c0-2.208,1.792-4,4-4Z"
+              transform="rotate(180 26 16)"
+              fill="#bf393b"
+            ></path>
+            <path
+              d="M27,4H5c-2.209,0-4,1.791-4,4V24c0,2.209,1.791,4,4,4H27c2.209,0,4-1.791,4-4V8c0-2.209-1.791-4-4-4Zm3,20c0,1.654-1.346,3-3,3H5c-1.654,0-3-1.346-3-3V8c0-1.654,1.346-3,3-3H27c1.654,0,3,1.346,3,3V24Z"
+              opacity=".15"
+            ></path>
+            <path
+              d="M27,5H5c-1.657,0-3,1.343-3,3v1c0-1.657,1.343-3,3-3H27c1.657,0,3,1.343,3,3v-1c0-1.657-1.343-3-3-3Z"
+              fill="#fff"
+              opacity=".2"
+            ></path>
           </svg>
           <span className="relative z-10 ml-1">Italiano</span>
         </button>
@@ -115,7 +182,9 @@ export default function Navbar() {
   return (
     <header
       className={`fixed w-full z-80 transition-all duration-300 ${
-        hasScrolled ? "bg-white/90 backdrop-blur shadow-sm py-3" : "bg-white/70 py-4"
+        hasScrolled
+          ? "bg-white/90 backdrop-blur shadow-sm py-3"
+          : "bg-white/70 py-4"
       }`}
       style={{ borderBottom: "1px solid rgba(0,0,0,0.1)" }}
     >
@@ -124,10 +193,10 @@ export default function Navbar() {
         <div className="flex items-center justify-between">
           {/* Logo - Left section */}
           <div className="w-1/4">
-            <Link href={language === 'en' ? '/' : '/it'}>
+            <Link href={language === "en" ? "/" : "/it"}>
               <div className="flex items-center space-x-3">
                 <Image
-                  src="/hotelLaPanoramicaLogo.jpg"
+                  src="/CSBLogo.jpg"
                   alt="Hotel La Panoramica Logo"
                   width={60}
                   height={60}
@@ -148,9 +217,11 @@ export default function Navbar() {
                   key={link.href}
                   href={link.href}
                   className={`font-medium text-gray-700 transition-colors hover:text-accent ${
-                    (pathname === link.href || 
-                     (pathname.replace(/^\/it/, '') === link.href.replace(/^\/it/, ''))) 
-                     ? "text-accent underline" : ""
+                    pathname === link.href ||
+                    pathname.replace(/^\/it/, "") ===
+                      link.href.replace(/^\/it/, "")
+                      ? "text-accent underline"
+                      : ""
                   }`}
                 >
                   {link.label}
@@ -165,7 +236,7 @@ export default function Navbar() {
             <div className="mr-4">
               <LanguageToggleSlider />
             </div>
-            
+
             {/* Social Icons */}
             <div className="flex items-center space-x-3">
               <a
@@ -193,7 +264,7 @@ export default function Navbar() {
             <div className="mr-2">
               <LanguageToggleSlider />
             </div>
-            
+
             <button
               className="text-gray-700 hover:text-accent transition-colors text-3xl"
               onClick={() => setIsMenuOpen((prev) => !prev)}
@@ -208,14 +279,14 @@ export default function Navbar() {
       {/* Mobile Menu */}
       <AnimatePresence>
         {isMenuOpen && (
-          <motion.div 
+          <motion.div
             className="md:hidden bg-white/90 backdrop-blur w-full shadow-inner"
             initial={{ height: 0, opacity: 0 }}
             animate={{ height: "auto", opacity: 1 }}
             exit={{ height: 0, opacity: 0 }}
             transition={{ duration: 0.3, ease: "easeInOut" }}
           >
-            <motion.div 
+            <motion.div
               className="flex flex-col px-6 py-4 space-y-4"
               initial={{ y: -20 }}
               animate={{ y: 0 }}
@@ -232,18 +303,20 @@ export default function Navbar() {
                     href={link.href}
                     onClick={() => setIsMenuOpen(false)}
                     className={`block font-medium text-lg transition-colors ${
-                      (pathname === link.href || 
-                      (pathname.replace(/^\/it/, '') === link.href.replace(/^\/it/, ''))) 
-                      ? "text-accent underline" : "text-gray-700 hover:text-accent"
+                      pathname === link.href ||
+                      pathname.replace(/^\/it/, "") ===
+                        link.href.replace(/^\/it/, "")
+                        ? "text-accent underline"
+                        : "text-gray-700 hover:text-accent"
                     }`}
                   >
                     {link.label}
                   </Link>
                 </motion.div>
               ))}
-              
+
               {/* Social Icons */}
-              <motion.div 
+              <motion.div
                 className="flex space-x-4 pt-4 justify-center"
                 initial={{ opacity: 0 }}
                 animate={{ opacity: 1 }}
