70a1831d6d509b9055bc34f61105d33422beec1e
Elijah Crofts <elicrofts@gmail.com>
2025-09-21 22:01:52 -0500

    added analytics

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -3,6 +3,7 @@ import "./globals.css";
 import Navbar from "@/components/layout/Navbar";
 import Footer from "@/components/layout/Footer";
 import { LanguageProvider } from "@/context/LanguageContext";
+import { Analytics } from "@vercel/analytics/next"; 
 
 const inter = Inter({ subsets: ["latin"] });
 
